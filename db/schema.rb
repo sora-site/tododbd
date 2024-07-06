f71529cc61f877db6406145ff82593d09c72e7fa
sora-site <soshi.1999.13@gmail.com>
2024-07-06 17:47:40 +0900

    projectモデル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_07_06_043859) do
+ActiveRecord::Schema[7.0].define(version: 2024_07_06_084137) do
+  create_table "projects", charset: "utf8", force: :cascade do |t|
+    t.date "registered_date", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_projects_on_user_id"
+  end
+
   create_table "users", charset: "utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -25,4 +33,5 @@ ActiveRecord::Schema[7.0].define(version: 2024_07_06_043859) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "projects", "users"
 end
