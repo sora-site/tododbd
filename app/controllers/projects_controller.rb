4618de86cd82f25ac0ad9309d1ef76cc26f2303e
sora-site <soshi.1999.13@gmail.com>
2024-07-07 15:47:00 +0900

    projectモデル、コントローラー編集

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,20 @@
 class ProjectsController < ApplicationController
   def index
   end
+
+  def new
+    @project = Project.new
+    @task = @project.task.build
+    @thing = @task.thing.build
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:project_name,
+                                    tasks_attributes: [:id, :task_name, :project_id, :_destroy,
+                                                       { things_attributes: [:id, :thing_name, :person_name, :start_time, :end_time,
+                                                                             :memo, :status_id, :task_id, :_destroy] }])
+          .merge(user_id: current_user.id, registered_date: Time.zone.today)
+  end
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,5 @@
 class Project < ApplicationRecord
-  has_many :tasks
+  has_many :tasks, dependent: :destroy
   accepts_nested_attributes_for :tasks,
                                 allow_destroy: true,
                                 reject_if: proc { |attributes| attributes['task_name'].blank? }
