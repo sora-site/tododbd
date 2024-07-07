1a762ff6a97c3353c01adab0a253240883f8f584
sora-site <soshi.1999.13@gmail.com>
2024-07-07 13:29:14 +0900

    アソシエーション定義

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,6 @@
 class Project < ApplicationRecord
   has_many :tasks
+  accepts_nested_attributes_for :tasks,
+                                allow_destroy: true,
+                                reject_if: proc { |attributes| attributes['task_name'].blank? }
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,8 @@
 class Task < ApplicationRecord
   has_many :things
+  accepts_nested_attributes_for :things,
+                                allow_destroy: true,
+                                reject_if: proc { |attributes|
+                                             attributes['thing_name'].blank? and attributes['person_name'].blank? and attributes['start_time'].blank? and attributes['end_time'].blank?
+                                           }
 end
