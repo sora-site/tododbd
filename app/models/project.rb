168058ca9ac0e36f1a289777ce80674dfd5d13af
sora-site <soshi.1999.13@gmail.com>
2024-07-13 19:35:25 +0900

    エラー修正対応

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,10 @@
 class Project < ApplicationRecord
   has_many :tasks, dependent: :destroy
 
+  def start_time
+    self. registered_date #self.の後はsimple_calendarに表示させるためのカラムを指定
+  end
+
   accepts_nested_attributes_for :tasks,
                                 allow_destroy: true,
                                 reject_if: proc { |attributes| attributes['task_name'].blank? }
