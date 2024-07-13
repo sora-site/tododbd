ca1af08881cea5b6b646725f2c98545e38a6a90c
sora-site <soshi.1999.13@gmail.com>
2024-07-13 15:20:03 +0900

    モデル修正（バリデーション・ログイン機能）

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,10 +1,11 @@
 class Task < ApplicationRecord
   belongs_to :project
-  has_many :things
+  has_many :things, dependent: :destroy
 
   accepts_nested_attributes_for :things,
                                 allow_destroy: true,
                                 reject_if: proc { |attributes|
-                                             attributes['thing_name'].blank? and attributes['person_name'].blank? and attributes['start_time'].blank? and attributes['end_time'].blank?
+                                             attributes['thing_name'].blank? and attributes['person_name'].blank? and
+                                               attributes['start_time'].blank? and attributes['end_time'].blank?
                                            }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,10 @@ class User < ApplicationRecord
   # has_many :rooms, through: :room_users
   # has_many :space-users
   # has_many :spaces, through: :space-users
+
+  VALID_PASSWORD_REGEX = /\A(?=.*[a-z]\d)[a-z\d]{6,128}\z/
+
+  validates :password, format: { with: VALID_PASSWORD_REGEX }
+  validates :birthday, presence: true
+  validates :nickname, presence: true
 end
