dc046a30ab63f508bead7ea3a658709346e9d283
sora-site <soshi.1999.13@gmail.com>
2024-07-14 19:21:28 +0900

    activehash修正

--- app/models/task_status.rb
+++ app/models/task_status.rb
@@ -1,7 +1,8 @@
 class TaskStatus < ActiveHash::Base
   self.data = [
     { id: 1, name: '---' }, { id: 2, name: '完了' },
-    { id: 3, name: '実施中' }, { id: 4, name: '未着手' }, { id: 5, name: 'スタック（実施不可）' }
+    { id: 3, name: '実施中' }, { id: 4, name: '未着手' },
+    { id: 5, name: '対応不要' }, { id: 6, name: 'スタック（実施不可）' }
   ]
 
   include ActiveHash::Associations
