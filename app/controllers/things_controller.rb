200cfd8fcff1e421640e9f0f03ebb2b4ec01b190
sora-site <soshi.1999.13@gmail.com>
2024-07-19 00:27:21 +0900

    Update things_controller.rb

--- app/controllers/things_controller.rb
+++ app/controllers/things_controller.rb
@@ -1,14 +1,22 @@
 class ThingsController < ApplicationController
   def edit
     @thing = Thing.find(params[:id])
+    session[:previous_url] = request.referer
   end
 
   def update
-    @project = Project.find(params[:id])
-    if @project.update(project_edit_params)
-      redirect_to request.referer
+    @thing = Thing.find(params[:id])
+    binding.pry
+    if @thing.update(thing_params)
+      redirect_to root_path
     else
-      render :show, status: :unprocessable_entity
+      render :edit, status: :unprocessable_entity
     end
+  end
+
+  private
+
+  def thing_params
+    params.require(:thing).permit(:id, :thing_name, :person_name, :start_time, :end_time, :memo, :status_id)
   end
 end
