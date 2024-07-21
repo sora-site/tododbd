5f40209cfb3fc89974974f3d6b6bd97594013267
sora-site <soshi.1999.13@gmail.com>
2024-07-21 19:21:26 +0900

    spaceモデル作成・er図修正

---
+++ db/migrate/20240721085902_create_spaces.rb
@@ -0,0 +1,8 @@
+class CreateSpaces < ActiveRecord::Migration[7.0]
+  def change
+    create_table :spaces do |t|
+      t.string     :space_name,              null: false
+      t.timestamps
+    end
+  end
+end
