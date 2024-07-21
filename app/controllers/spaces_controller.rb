309995e1d9d14765425f9b06bb0b986cd39bea0b
sora-site <soshi.1999.13@gmail.com>
2024-07-21 20:29:48 +0900

    コントローラー・view修正反映

--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -1,7 +1,22 @@
 class SpacesController < ApplicationController
   def new
+    @space = Space.new
+    @users = User.where.not(id: current_user.id)
   end
 
   def create
+    # binding.pry
+    @space = Space.new(space_params)
+    if @space.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def room_params
+    params.require(:space).permit(:space_name, user_ids: [])
   end
 end
