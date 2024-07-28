83bf324e69dbd3ac073899c49cb784c857deab40
sora-site <soshi.1999.13@gmail.com>
2024-07-28 22:50:38 +0900

    user_ids配列バリデーション

--- app/models/space.rb
+++ app/models/space.rb
@@ -1,8 +1,17 @@
 class Space < ApplicationRecord
-  has_many :space_users
+  has_many :space_users, dependent: :destroy
   has_many :users, through: :space_users
 
   with_options presence: true do
     validates :space_name
+  end
+  validate :user_ids_must_be_at_least_two
+
+  private
+
+  def user_ids_must_be_at_least_two
+    return unless user_ids.length < 2
+
+    errors.add(:user_ids, 'を１人以上選択してください')
   end
 end
